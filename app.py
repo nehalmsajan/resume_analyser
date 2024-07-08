3ae8dcbb579e09553875f016fcbcc6e004df79e6
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2024-07-08 21:35:46 +0530

    changes made to fe and backend testing required

--- app.py
+++ app.py
@@ -7,45 +7,109 @@ from dotenv import load_dotenv
 import json
 from PIL import Image
 
-load_dotenv() ## load all our environment variables
+load_dotenv()  # Load all our environment variables
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-def get_gemini_repsonse(input):
-    model=genai.GenerativeModel('gemini-pro')
-    response=model.generate_content(input)
+def get_gemini_response(input):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input)
     return response.text
 
 def input_pdf_text(uploaded_file):
-    reader=pdf.PdfReader(uploaded_file)
-    text=""
+    reader = pdf.PdfReader(uploaded_file)
+    text = ""
     for page in range(len(reader.pages)):
-        page=reader.pages[page]
-        text+=str(page.extract_text())
+        page = reader.pages[page]
+        text += str(page.extract_text())
     return text
 
-#Prompt Template
-
-input_prompt="""
-As an experienced ATS (Applicant Tracking System), proficient in the technical domain encompassing Software Engineering, Data Science, Data Analysis, Big Data Engineering, Web Developer, Mobile App Developer, DevOps Engineer, Machine Learning Engineer, Cybersecurity Analyst, Cloud Solutions Architect, Database Administrator, Network Engineer, AI Engineer, Systems Analyst, Full Stack Developer, UI/UX Designer, IT Project Manager, and additional specialized areas, your objective is to meticulously assess resumes against provided job descriptions. In a fiercely competitive job market, your expertise is crucial in offering top-notch guidance for resume enhancement. Assign precise matching percentages based on the JD (Job Description) and meticulously identify any missing keywords with utmost accuracy.
-resume: {text}
-description: {jd)
-I want the response in the following structure:
-The first line indicates the percentage match with the job description (JD).
-The second line presents a list of missing keywords.
-The third section provides a profile summary.
-Mention the title for all the three sections.
-While generating the response put some space to separate all the three sections.
-"""
-
-## streamlit UI
+# Streamlit UI
 st.set_page_config(page_title="Resume ATS Tracker", layout="wide")
+
+# Custom CSS to change the background color to white, text color to black, and add fonts and styling
+st.markdown(
+    """
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
+
+    .stApp {
+        background-color: white;
+        color: black;
+        font-family: 'Roboto', sans-serif;
+    }
+    h1, h2, h3, h4, h5, h6 {
+        color: black;
+        font-family: 'Roboto', sans-serif;
+    }
+    p, div, span {
+        color: black;
+        font-family: 'Roboto', sans-serif;
+    }
+    .stImage {
+        margin: auto;
+        display: block;
+        margin: 12px 12px;
+        border-radius:33px;
+    }
+    .stButton>button {
+        background-color: #0066cc;
+        color: white !important;
+        border: none;
+        border-radius: 5px;
+        padding: 10px 20px;
+        font-size: 16px;
+        cursor: pointer;
+    }
+    .stButton>button:hover {
+        background-color: #005bb5;
+    }
+    .header-title {
+        text-align: center;
+        font-weight: 700;
+        font-size: 2.5em;
+    }
+    .sub-header {
+        text-align: center;
+        font-weight: 400;
+        font-size: 1.5em;
+    }
+    .description {
+        text-align: justify;
+        font-weight: 300;
+        font-size: 1.2em;
+    }
+    .offerings {
+        font-weight: 300;
+        font-size: 1.2em;
+    }
+    .faq {
+        background-color: white !important;
+        font-weight: 300;
+        font-size: 1rem;
+        margin: 9px;
+    }
+    .faq-question {
+        font-weight: 400;
+        font-size: 1.2em;
+    }
+    .faq-answer {
+        font-weight: 300;
+        font-size: 1.2em;
+        margin-bottom: 10px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 avs.add_vertical_space(4)
-col1, col2 = st.columns([3,2])
+col1, col2 = st.columns([3, 2])
 with col1:
     st.title("CareerCraft")
     st.header("Navigate the Job Market with Confidence!")
-    st.markdown("""<p style='text-align: justify; '>
+    st.markdown("""
+                <p class='description'>
                 Introducing CareerCraft, an ATS-Optimized Resume Analyzer your ultimate solution 
                 for optimizing job applications and accelerating career growth. Our innovative platform 
                 leverages advanced ATS technology to provide job seekers with valuable insights into their resumes' 
@@ -53,57 +117,79 @@ with col1:
                 CareerCraft empowers users to stand out in today's competitive job market. Streamline your job application process, 
                 enhance your skills, and navigate your career path with confidence. Join CareerCraft today and unlock new opportunities 
                 for professional success!
-                </p>""", unsafe_allow_html=True)
+                </p>
+                """, unsafe_allow_html=True)
 with col2:
     st.image('https://cdn.dribbble.com/userupload/12500996/file/original-b458fe398a6d7f4e9999ce66ec856ff9.gif', use_column_width=True)
+
 avs.add_vertical_space(10)
 
-coll, col2 = st.columns([3,2])
+col1, col2 = st.columns([3, 2])
 with col2:
-    st.header("Wide Range of offerings")
-    st.write('ATS-Optimized Resume Analysis')
-    st.write('Resume Optimization')
-    st.write('Skill Enhancement')
-    st.write('Career Progression Guidance')
-    st.write('Tailored Profile Summaries')
-    st.write('Streamlined Application Process')
-    st.write('Personalized Recommendations')
-    st.write('Efficient Career Navigation')
+    st.header("Wide Range of Offerings")
+    st.markdown("""
+                <ul class='offerings'>
+                    <li>ATS-Optimized Resume Analysis</li>
+                    <li>Resume Optimization</li>
+                    <li>Skill Enhancement</li>
+                    <li>Career Progression Guidance</li>
+                    <li>Tailored Profile Summaries</li>
+                    <li>Streamlined Application Process</li>
+                    <li>Personalized Recommendations</li>
+                    <li>Efficient Career Navigation</li>
+                </ul>
+                """, unsafe_allow_html=True)
 with col1:
     img1 = Image.open("images/icon1.png")
-    st.image(img1, use_column_width=True)
+    st.image(img1, use_column_width=True, caption="Optimize Your Resume")
 
 avs.add_vertical_space(10)
 
-col1, col2 = st.columns([3,2])
+col1, col2 = st.columns([3, 2])
 with col1:
-    st.markdown("<h1 style='text-align: center; '>Embark on Your Career Adventure</h1>", unsafe_allow_html=True)
-    jd=st.text_area("Paste the Job Description")
-    uploaded_file=st.file_uploader("Upload Your Resume", type="pdf", help="Please uplaod the pdf")
+    st.markdown("<h1 class='header-title'>Embark on Your Career Adventure</h1>", unsafe_allow_html=True)
+    jd = st.text_area("Paste the Job Description")
+    uploaded_file = st.file_uploader("Upload Your Resume", type="pdf", help="Please upload the PDF")
     submit = st.button("Submit")
     if submit:
-        if uploaded_file is not None: 
-            text=input_pdf_text(uploaded_file) 
-            response=get_gemini_repsonse(input_prompt) 
-            st.subheader (response)
+        if uploaded_file is not None and jd:
+            text = input_pdf_text(uploaded_file)
+            input_prompt = f"""
+            As an experienced ATS (Applicant Tracking System), proficient in the technical domain encompassing Software Engineering, Data Science, Data Analysis, Big Data Engineering, Web Developer, Mobile App Developer, DevOps Engineer, Machine Learning Engineer, Cybersecurity Analyst, Cloud Solutions Architect, Database Administrator, Network Engineer, AI Engineer, Systems Analyst, Full Stack Developer, UI/UX Designer, IT Project Manager, and additional specialized areas, your objective is to meticulously assess resumes against provided job descriptions. In a fiercely competitive job market, your expertise is crucial in offering top-notch guidance for resume enhancement. Assign precise matching percentages based on the JD (Job Description) and meticulously identify any missing keywords with utmost accuracy.
+            resume: {text}
+            description: {jd}
+            I want the response in the following structure:
+            The first line indicates the percentage match with the job description (JD).
+            The second line presents a list of missing keywords.
+            The third section provides a profile summary.
+            Mention the title for all the three sections.
+            While generating the response put some space to separate all the three sections.
+            """
+            response = get_gemini_response(input_prompt)
+            st.subheader(response)
 with col2:
-    img2 = Image.open("images/icon2.png") 
-    st.image(img2, use_column_width=True)
+    img2 = Image.open("images/icon2.png")
+    st.image(img2, use_column_width=True, caption="Career Guidance")
 
 avs.add_vertical_space(10)
-
 
 col1, col2 = st.columns([2, 3])
 with col2:
-    st.markdown("<h1 style='text-align: center;'>FAQ</h1>", unsafe_allow_html=True)
-    st.write("Question: How does CareerCraft analyze resumes and job descriptions?")
-    st.write("""Answer: CareerCraft uses advanced algorithms to analyze resumes and job descriptions, identifying key keywords and assessing compatibility between the two.""")
-    avs.add_vertical_space(3)
-    st.write("Question: Can CareerCraft suggest improvements for my resume?")
-    st.write("""Answer: Yes, CareerCraft provides personalized recommendations to optimize your resume for specific job openings, including suggestions for missing keywords and alignment with desired job roles.""")
-    avs.add_vertical_space(3)
-    st. write("Question: Is CareerCraft suitable for both entry-level and experienced professionals?") 
-    st.write("""Answer: Absolutely! CareerCraft caters to job seekers at all career stages, offering tailored insights and guidance to enhance their resumes and advance their careers.""")
+    st.markdown("<h1 class='sub-header'>FAQ</h1>", unsafe_allow_html=True)
+    st.markdown("""
+                <div  class="faq">
+                    <p class='faq-question'>Question: How does CareerCraft analyze resumes and job descriptions?</p>
+                    <p class='faq-answer'>Answer: CareerCraft uses advanced algorithms to analyze resumes and job descriptions, identifying key keywords and assessing compatibility between the two.</p>
+                </div>
+                <div class="faq">
+                    <p class='faq-question'>Question: Is CareerCraft suitable for both entry-level and experienced professionals?</p>
+                    <p class='faq-answer'>Answer: Absolutely! CareerCraft caters to job seekers at all career stages, offering tailored insights and guidance to enhance their resumes and advance their careers.</p>
+                </div>
+                <div class="faq">
+                    <p class='faq-question'>Question: Can CareerCraft suggest improvements for my resume?</p>
+                    <p class='faq-answer'>Answer: Yes, CareerCraft provides personalized recommendations to optimize your resume for specific job openings, including suggestions for missing keywords and alignment with desired job roles.</p>
+                </div>
+                """, unsafe_allow_html=True)
 with col1:
     img3 = Image.open("images/icon3.png")
-    st.image(img3, use_column_width=True)
+    st.image(img3, use_column_width=True, caption="Advanced Analysis")
